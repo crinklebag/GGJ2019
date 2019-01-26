9039f9ce54e824e77a38d9d5f9bb567fce049b5b
davetamayo <djbtamayo@gmail.com>
2019-01-26 17:46:11 -0500

    Adding in new realtor dialogue functionality, new fonts, and the base Text Mesh Pro package

--- Assets/Scripts/RegularNPCDialogue.cs
+++ Assets/Scripts/RegularNPCDialogue.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RegularNPCDialogue : MonoBehaviour
+public class RegularNPCDialogue : MonoSingleton<RegularNPCDialogue>
 {
 
     [Header("NPC Line Buckets")]
