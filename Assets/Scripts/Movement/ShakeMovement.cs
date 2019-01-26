3324bd31e37d6e3713857f9ef2b9cd6cadcf7c13
AustinEathorne <austineathorne@gmail.com>
2019-01-26 13:55:07 -0500

    Implement basic shake movement.

--- Assets/Scripts/Movement/ShakeMovement.cs
+++ Assets/Scripts/Movement/ShakeMovement.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.Numerics;
 using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
 
 [Serializable]
 public class ShakeMovement : Movement
 {
-    [SerializeField] private float _shakeDistance;
+    [SerializeField] private Transform _modelTransform;
+    [SerializeField] private float _hoverRange;
+    [SerializeField] private float _hoverSpeed;
+    
+    private Vector3 _startPosition;
+
+
+
+    public void Awake()
+    {
+        _startPosition = _modelTransform.localPosition;
+    }
 
     public override void Move(Vector3 input)
     {
+        transform.localPosition += input * _speed * Time.deltaTime;
 
+        // Move child object up and down
+        _modelTransform.localPosition = new Vector3(
+            _modelTransform.localPosition.x,
+            _startPosition.x + (Mathf.Sin(Time.time * _hoverSpeed) * _hoverRange),
+            _modelTransform.localPosition.z);
     }
 }
